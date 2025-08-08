ec5e811062475521714da9972e28a7ea39776bf5
JosephRidge <josephridge18@gmail.com>
2025-08-08 20:20:03 +0300

    interface addition

--- app/src/main/java/com/jayr/supabasecrud/data/models/Todo.kt
+++ app/src/main/java/com/jayr/supabasecrud/data/models/Todo.kt
@@ -1,13 +1,11 @@
 package com.jayr.supabasecrud.data.models
 
 data class Todo(
-    val id:Int? =null,
-    val createdAt:String? = null,
+    val id:Int? =null,// will be used a a Primary key in supabase
+    val createdAt:Long? = null, // wil be automatically set to now() everytime we do a creation
     val title:String,
     val description:String,
     val media: String, //  store images or video
     val isComplete: Boolean = false, // default to false
     val dueDate:Long, // store as unix timestamp
-    val dueTime:Long, // store as timestamp
-
 )
---
+++ app/src/main/java/com/jayr/supabasecrud/data/repository/TodoService.kt
@@ -0,0 +1,11 @@
+package com.jayr.supabasecrud.data.repository
+
+import com.jayr.supabasecrud.data.models.Todo
+
+interface TodoService {
+    suspend fun createTask(todo: Todo):Todo? // create task
+    suspend fun getAllTasks(): List<Todo> // read all tasks
+    suspend fun getTask(id:Int): Todo? // read one task
+    suspend fun updateTask(todo: Todo):Todo? // update task
+    suspend fun deleteTask(id:Int): Boolean // delete task and return true or false based on success
+}
